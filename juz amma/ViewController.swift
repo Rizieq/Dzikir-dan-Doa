d0485d8129058a55f437c65e72020a6f74533e66
Macbook Pro <macbookpro@rizieq-macbook.local>
2018-10-10 15:49:53 +0700

    Initial Commit

---
+++ juz amma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  juz amma
+//
+//  Created by Macbook Pro on 10/10/18.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
